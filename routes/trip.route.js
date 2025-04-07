a414cd044c6a457318dbe3cd6ae4e52cfb9b6b00
KCD <kok.chengda@gmail.com>
2025-04-07 22:56:42 +0800

    update prompt

--- routes/trip.route.js
+++ routes/trip.route.js
@@ -1,27 +1,41 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import express from "express"
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/generate', async (req, res) => {
-    const { prompt } = req.body;
+router.post("/generate", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ error: "request body is empty" });
+  }
+  const {
+    destinations,
+    lengthOfTrip,
+    arrivalAirport,
+    departureAirport,
+    timeOfArrival,
+    timeOfDeparture,
+  } = req.body;
 
-    if (!prompt) {
-        return res.status(400).json({ error: 'No prompt provided' });
-    }
+  const prompt = `Create a travel itinerary for Japan based on the following details: Destination(s): ${destinations.join(
+    ","
+  )}, Length of Trip: ${lengthOfTrip} day(s), ${
+    arrivalAirport && `Arrival Airport: ${arrivalAirport},`
+  } ${departureAirport && `Departure Airport: ${departureAirport},`} ${
+    timeOfArrival ? `Time of Arrival: timeOfArrival,` : ""
+  } ${timeOfDeparture ? `Time of Departure: timeOfDeparture` : ""}`;
 
-    try {
-        const genAI = new GoogleGenerativeAI(process.env.GEMINI);
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" })
-        
-        console.log(prompt)
-        const result = await model.generateContent(prompt)
-        const response = result.response
-        res.status(200).json(response.text())
-    } catch (error) {
-        console.error('Error fetching response from Gemini:', error);
-        res.status(500).json({ error: 'Failed to get response from Gemini API' });
-    }
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI);
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+    console.log(prompt);
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    res.status(200).json(response.text());
+  } catch (error) {
+    console.error("Error fetching response from Gemini:", error);
+    res.status(500).json({ error: "Failed to get response from Gemini API" });
+  }
 });
 
-export default router
+export default router;
